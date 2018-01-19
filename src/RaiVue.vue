06c8a5ef15973de2c49c42c12a8031f32a9ecf46
Joshua Porter <joshporter1@gmail.com>
2018-01-19 00:54:34 -0500

    Added brainblocks code block with defaults.

--- src/RaiVue.vue
+++ src/RaiVue.vue
@@ -3,14 +3,37 @@
     <h1 class="raivue__title">RaiVue World!</h1>
     <span>addr: {{address}}</span>
     <span>amt: {{amount}}</span>
+    <div id="raiblocks-button"></div>
   </div>
 </template>
 
 <script>
 import * as brainblocks from '../lib/brainblocks/dist/brainblocks'
-console.log(brainblocks)
+
 export default {
-  props: ['address', 'amount', 'onSuccess']
+  props: ['address', 'amount', 'onSuccess'],
+  mounted () {
+    const _this = this
+    setTimeout(function () {
+      _this.initialize()
+    })
+  },
+  methods: {
+    initialize () {
+      brainblocks.Button.render({
+        // Pass in payment options
+        payment: {
+          destination: 'xrb_164xaa1ojy6qmq9e8t94mz8izr4mkf1sojb6xrmstru5jsif48g5kegcqg7y',
+          currency: 'rai',
+          amount: 1000
+        },
+        // Handle successful payments
+        onPayment: function (data) {
+          console.log('Payment successful!', data)
+        }
+      }, '#raiblocks-button')
+    }
+  }
 }
 </script>
 
