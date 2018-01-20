ac22df56efdde239df7701979c8108229534ddc1
Joshua Porter <joshporter1@gmail.com>
2018-01-19 20:07:00 -0500

    Updated plugin export.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import RaiVue from './RaiVue.vue'
 
-function plugin (Vue) {
+function Raivue (Vue) {
   Vue.component('raivue', RaiVue)
 }
 
 // Install by default if using the script tag
 if (typeof window !== 'undefined' && window.Vue) {
-  window.Vue.use(plugin)
+  window.Vue.use(Raivue)
 }
 
-export default plugin
-const version = '__VERSION__'
-// Export all components too
-export {
-  RaiVue,
-  version
-}
+export default Raivue
+// const version = '__VERSION__'
+// // Export all components too
+// export {
+//   RaiVue,
+//   version
+// }
