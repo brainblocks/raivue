4b3374de6a382c8abde63d0eef5d5b60ec229621
Joshua Porter <joshporter1@gmail.com>
2018-01-20 14:25:57 -0500

    Switched from rollup to webpack for builds.

--- test/specs/RaiVue.spec.js
+++ test/specs/RaiVue.spec.js
@@ -6,6 +6,8 @@ describe('RaiVue.vue', function () {
     const vm = createVM(this, `
 <RaiVue address="xrb_3ui4sg4jjdxsfwshjcbkjnthdzmtbgxe7pa597cxsa4aamkkj3b8dmeome4i" :amount="250000"></RaiVue>
 `, { components: { RaiVue }})
-    vm.$el.querySelector('#raivue__button').id.should.equal('raivue__button')
+    setTimeout(function () {
+      vm.$el.querySelector('#raivue__button').children.length.should.equal(1)
+    }, 1000)
   })
 })
